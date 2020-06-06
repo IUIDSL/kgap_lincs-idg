c0d083e10d8a036e7b06a9ef559fcfa2a782719b
Jeremy Yang <jeremyjyang@gmail.com>
2020-06-06 15:58:05 -0600

    Ok.

--- sh/Go_dc_PD.sh
+++ sh/Go_dc_PD.sh
@@ -17,7 +17,10 @@ python3 -m BioClients.drugcentral.Client get_indication_structures \
 	--i dc_indications_PD.concept_id \
 	--o dc_structures_PD.tsv
 cat dc_structures_PD.tsv |sed -e '1d' \
-	|awk -F '\t' '{print $3}' |sort -u \
+	|awk -F '\t' '{print $5 "\t" $3 "\t" $4}' |sort -u \
+	>dc_structures_PD.smiles
+cat dc_structures_PD.smiles \
+	|awk -F '\t' '{print $2}' \
 	>dc_structures_PD.struct_id
 #
 python3 -m BioClients.drugcentral.Client get_structure_ids \
