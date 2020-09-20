5a2cfd031a8172bcc1533a6828d7ca169087b7d1
Jeremy Yang <jeremyjyang@gmail.com>
2020-09-20 17:12:16 -0600

    Ok.

--- python/ROC_analysis.py
+++ python/ROC_analysis.py
@@ -50,28 +50,69 @@ b = b.reset_index(level=1, drop=True)
 dcgenes = dcgenes.drop(columns=["genes"]).join(b, how="left")
 print(dcgenes.head(12))
 
+NeoUser=""; NeoPass=""; NeoHost=""; NeoPort="";
 with open(os.environ["HOME"]+"/.neo4j.sh") as fin:
-    NeoUser = ""
-    NeoPass = ""
     while True:
         line = fin.readline()
         if not line: break
-        if re.match('.*NEO4J_USERNAME=', line):
-            NeoUser = re.sub(r'^.*NEO4J_USERNAME="?([^"]*)"?$', r'\1', line.rstrip())
-        elif re.match('.*NEO4J_PASSWORD=', line):
-            NeoPass = re.sub(r'^.*NEO4J_PASSWORD="?([^"]*)"?$', r'\1', line.rstrip())
+        if re.match('[^#]*NEO4J_USERNAME=', line):
+            NeoUser = re.sub(r'^[^#]*NEO4J_USERNAME="?([^"]*)"?$', r'\1', line.rstrip())
+        elif re.match('[^#]*NEO4J_PASSWORD=', line):
+            NeoPass = re.sub(r'^[^#]*NEO4J_PASSWORD="?([^"]*)"?$', r'\1', line.rstrip())
+        elif re.match('[^#]*NEO4J_HOST=', line):
+            NeoHost = re.sub(r'^[^#]*NEO4J_HOST="?([^"]*)"?$', r'\1', line.rstrip())
+        elif re.match('[^#]*NEO4J_PORT=', line):
+            NeoPort = re.sub(r'^[^#]*NEO4J_PORT="?([^"]*)"?$', r'\1', line.rstrip())
+    print("NeoHost: \"{}\"; NeoPort: \"{}\"\n".format(NeoHost, NeoPort))
     print("NeoUser: \"{}\"; NeoPass: \"{}\"\n".format(NeoUser, NeoPass))
 
-uri = "neo4j://hoffmann.data2discovery.net:7695"
-db = neo4j.GraphDatabase.driver(uri, auth= (NeoUser, NeoPass))
+url = ("neo4j://{}:{}".format(NeoHost, NeoPort))
+db = neo4j.GraphDatabase.driver(url, auth=(NeoUser, NeoPass))
 session = db.session()
 
-cqlurl = "https://raw.githubusercontent.com/IUIDSL/kgap_lincs-idg/master/cql/pd-adamic-adar.cql"
-cql = requests.get(cqlurl).text
+#cqlurl = "https://raw.githubusercontent.com/IUIDSL/kgap_lincs-idg/master/cql/pd-adamic-adar.cql"
+#cql = requests.get(cqlurl).text
+
+cql="""\
+MATCH p=(d:Drug)-[]-(s:Signature)-[]-(g:Gene)
+WHERE
+d.pubchem_cid='2130'
+OR d.pubchem_cid='6005'
+OR d.pubchem_cid='1201549'
+OR d.pubchem_cid='2381'
+OR d.pubchem_cid='31101'
+OR d.pubchem_cid='34359'
+OR d.pubchem_cid='2911'
+OR d.pubchem_cid='30843'
+OR d.pubchem_cid='92974'
+OR d.pubchem_cid='5281081'
+OR d.pubchem_cid='6047'
+OR d.pubchem_cid='23497'
+OR d.pubchem_cid='4167'
+OR d.pubchem_cid='4601'
+OR d.pubchem_cid='47811'
+OR d.pubchem_cid='4850'
+OR d.pubchem_cid='119570'
+OR d.pubchem_cid='4919'
+OR d.pubchem_cid='77991'
+OR d.pubchem_cid='5095'
+OR d.pubchem_cid='59227'
+OR d.pubchem_cid='26757'
+OR d.pubchem_cid='5374'
+OR d.pubchem_cid='4659569'
+OR d.pubchem_cid='5572'
+OR d.pubchem_cid='3052776'
+OR d.pubchem_cid='5311037'
+OR d.pubchem_cid='131682'
+OR d.pubchem_cid='10071196'
+OR d.pubchem_cid='76966913'
+WITH g, COUNT(DISTINCT s) AS score RETURN g.id, g.name, score
+ORDER BY score DESC
+"""
 print("CQL: {}\n".format(cql))
 cdf = cypher2df(cql)
 cdf.head(10)
 
-plt = ROCplotter(cdf, dcgenes[(dcgenes.moa)], gene_tag_r = "gd.name", gene_tag_v="gene", score_tag = "score")
+plt = ROCplotter(cdf, dcgenes[(dcgenes.moa)], gene_tag_r = "g.name", gene_tag_v="gene", score_tag = "score")
 plt.title('KGAP-LINCS ROC vs DrugCentral PD Genes (MoA)')
 plt.show()
